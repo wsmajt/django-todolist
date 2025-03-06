0eafc1e64910b7fcc177df4f341bce0b1772167d
wsmajt <kacpermusz@gmail.com>
2025-03-06 23:39:30 +0100

    Initial commit

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,11 @@
+﻿<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
+
+<template>
+  <div class="text-6xl text-zinc-50 font-sans font-bold flex flex-col justify-center items-center w-full">
+    <h1>Welcome to the home page</h1>
+  </div>
+</template>
---
+++ sim/forms.py
@@ -0,0 +1,2 @@
+﻿from django import forms
+from django.contrib.auth.models import User
